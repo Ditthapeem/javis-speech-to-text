c05df7571a105bd367df5b2b87074948c19d29c0
Ditthapong Lakagul <lakagul.peem@gmail.com>
2024-01-15 10:15:49 +0700

    initial commit

---
+++ javis.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+def record_text():
+    while(1):
+        try:
+            with sr.Microphone() as source2:
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+                audio2 =r.listen(source2)
+                my_text = r.recognize_google(audio2)
+
+                return my_text
+
+        except sr.RequestError as e:
+            print(f"Could not request result; {e}")
+        except sr.UnknownValueError as e:
+            print("Unknown error occurred")
+
+def output_text(text):
+    f = open("output.txt", "a")
+    f.write(text)
+    f.write('\n')
+    f.close()
+
+while(True):
+    text = record_text()
+    output_text(text)
+
+    print("Text wrote")
